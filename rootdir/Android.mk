2b3f4bf6ed2c283c7cc88943c8084ba8bae48c2c
Sergey Vasiliev <sdv22@live.ru>
2023-03-14 07:18:58 +0300

    Baikal Audio and Effects

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -72,3 +72,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.thermal.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.custom.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.custom.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
